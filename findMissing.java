e3dd59b6596ed738b7aaf38166b37a409a767fac
Özge Havva Şahin <ozgehavva.02@gmail.com>
2024-03-04 14:51:40 +0300

    Finding majority algorithm

--- findMissing.java
+++ findMissing.java
@@ -8,6 +8,7 @@ class deneme{
     static int findMiss(int[] arr) {
         Arrays.sort(arr);
         int miss = -1;
+        
         for (int i = 0; i < arr.length-1; i++) {
             if(arr[i]+1 != arr[i+1]){
                 miss = arr[i]+1;
